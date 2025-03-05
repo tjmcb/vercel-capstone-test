28c0b265d6b05b99485fa657d522750377c16217
Jonathan Zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2025-03-05 12:30:24 -0500

    Modify `Lobby` and `Player` models for new game

--- backend/hello.py
+++ backend/hello.py
@@ -1,7 +1,7 @@
 import random
 
 from fastapi import FastAPI, HTTPException, WebSocket
-from models import Lobby, Message, Player
+from models import Message
 
 app = FastAPI()
 
@@ -43,27 +43,6 @@ def join_lobby(code: str, player: str) -> dict:
         raise HTTPException(status_code=404, detail="Lobby not found")
     lobby["players"].append(player)
     return {"code": code, "players": lobby["players"]}
-
-
-@app.get("/testGameState/")
-async def test_game_state() -> Lobby:
-    """Simulates and returns a test game state."""
-    # create lobby
-    game = Lobby(id=0)
-
-    # add players to lobby
-    player1 = Player(id=1, name="player 1")
-    game.players[player1.id] = player1
-    game.player_count += 1
-    player2 = Player(id=2, name="player 2")
-    game.players[player2.id] = player2
-    game.player_count += 1
-
-    # start game
-    game.started = True
-    game.current_turn = player1.id
-
-    return game
 
 
 @app.get("/")
--- backend/models.py
+++ backend/models.py
@@ -12,28 +12,13 @@ class Lobby(BaseModel):
         id (int): The lobby's internal ID
         code (str): The code used to join the lobby
         players (dict): A list of player objects participating in the game
-        player_count (int): Number of players in game, max 4
-
         started (bool): Whether the game has started
-
-        current_turn (int): The ID of the player whose turn it current is
-
-    Methods:
-        get_deck_count(): Returns size of the deck
     """
 
     id: int
     code: str = "ABC"
     players: dict = dict()
-    player_count: int = 0
-
     started: bool = False
-
-    current_turn: int = -1
-    # deck: list[str] = make_deck(4)
-
-    # def get_deck_count(self):
-    #     return len(self.deck)
 
 
 class Player(BaseModel):
@@ -44,24 +29,11 @@ class Player(BaseModel):
         id (int): The player's internal ID
         code (str): The code used to join the game
         socket (): TODO: A web socket object (this is probably its own class idk yet)
-
-        cards (list[str]): A list of cards this player has, private
-        books (list[str]): A list of books this player has, visible to other players
-
-    Methods:
-        get_card_count(): Returns player's hand size
     """
 
     id: int
     name: str = "Unnamed Player"
     socket: None = None
-
-    cards: list[str] = []
-    books: list[str] = []
-
-    def get_card_count(self) -> int:
-        """Returns the number of cards in the player's hand."""
-        return len(self.cards)
 
 
 class MessageKind(StrEnum):
